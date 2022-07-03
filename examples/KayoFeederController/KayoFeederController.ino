68ced98052420a2574a4032c17970fdfb4b2ddd5
xuanngoc1992 <xuanngoc1992@gmail.com>
2022-07-03 23:53:03 +0700

    Upload Firmware OTA OK, WebServer_Config testing!

--- examples/KayoFeederController/KayoFeederController.ino
+++ examples/KayoFeederController/KayoFeederController.ino
@@ -3,8 +3,43 @@
 #include "Vietduino_Button.h"
 #include "PinVar.h"
 
+#include "OTA_AccessPoint.h"
+
+// #include "Arduino.h"
+// #if defined(ESP8266)
+//   #include <ESP8266WiFi.h>
+//   #include <ESPAsyncTCP.h>
+// #elif defined(ESP32)
+//   #include <WiFi.h>
+//   #include <AsyncTCP.h>
+// #endif
+
+// #include <ESPAsyncWebServer.h>
+// #include <AsyncElegantOTA.h>
+
 Vietduino_Led feeder(PIN_FEEDER, SIG_ACTIVE_FEEDER);
 Vietduino_Button ds30c4;
+
+// const char* ssid = "noFree";
+// const char* password = "likehshop";
+
+// AsyncWebServer server(80);
+
+// void OTA_AccessPoint_Init(){
+//     /* You can remove the password parameter if you want the AP to be open. */
+//     WiFi.mode(WIFI_AP_STA);
+
+//     WiFi.softAP(ssid, password);
+
+//     IPAddress myIP = WiFi.softAPIP();
+
+//     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+//         request->send(200, "text/plain", "Hi! This is a sample response.");
+//     });
+
+//     AsyncElegantOTA.begin(&server);    // Start AsyncElegantOTA
+//     server.begin();
+// }
 
 void setup()
 {
@@ -19,9 +54,11 @@ void setup()
     });
 
     ds30c4.attachRelease([](Vietduino_Task * me){
-        feeder.write(2000);
+        feeder.write(4000);
         Serial.println(F("Start FEEDER"));
     });
+
+    OTA_AccessPoint_Init();
 }
 
 void loop()
---
+++ examples/KayoFeederController/WebServer_Config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Arduino.h"
+
+extern void Config_Init();
